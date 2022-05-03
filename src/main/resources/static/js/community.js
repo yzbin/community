297a028f87dfcfd97fee2d73162717d0e430e56f
yzbin <jiabin126email@126.com>
2022-05-03 10:45:00 +0800

    实现问题回复页面提交

---
+++ src/main/resources/static/js/community.js
@@ -0,0 +1,25 @@
+function post(){
+    var questionsId = $("#question_id").val();
+    var content = $("#comment_content").val();
+    $.ajax({
+        type: "POST",
+        url: "/comment",
+        contentType:"application/json",
+        data: JSON.stringify(
+            {
+                "parentId":questionsId,
+                "content":content,
+                "type":1
+            }
+        ),
+        success: function (response){
+            if(response.code== 200){
+                $("#comment_section").hide();
+            }else {
+                alert(response.message);
+            }
+            console.log(response)
+        },
+        dataType: "json"
+    });
+}
